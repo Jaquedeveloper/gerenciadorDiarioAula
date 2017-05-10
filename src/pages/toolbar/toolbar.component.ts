aa6dfec5b45243c3ad98ceca71416a4fc66f98f3
Jales Cardoso <jales@logoseducacao.com.br>
2017-05-10 11:07:05 -0300

    code para accordion

--- src/pages/toolbar/toolbar.component.ts
+++ src/pages/toolbar/toolbar.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import {IMyOptions} from 'mydatepicker';
 
+declare var componentHandler: any;
+
 @Component({
   selector: 'tool-bar',
   templateUrl: 'toolbar.component.html'
 })
 export class Toolbar {
-
+/*
+ISSO FAZ FUNCIONAR O ACCORDION
+  */
+  private abaHistoricoJsLibsStarted: boolean = false;
+  ngAfterViewChecked() {
+    componentHandler.upgradeDom(); // mdl
+    let escola_info :any = document.getElementById("escola-info");
+    if (!this.abaHistoricoJsLibsStarted && escola_info && escola_info.length) {
+      escola_info.collapsible(); // materializecss
+      this.abaHistoricoJsLibsStarted = true;
+    }
+  }
 }
